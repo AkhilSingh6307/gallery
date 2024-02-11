9f38e88f2d0533200afdac0dac29d8acdaa27d79
Akhil Singh <158085831+AkhilSingh6307@users.noreply.github.com>
2024-02-11 16:21:48 +0530

    gallery

---
+++ main.js
@@ -0,0 +1,15 @@
+// Fancybox Configuration
+$('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+      "slideShow",
+      "thumbs",
+      "zoom",
+      "fullScreen",
+      "share",
+      "download",
+      "close"
+    ],
+    loop: false,
+    protect: true
+  });
+  
